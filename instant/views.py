0523001901db004d4f536aae4a67f781352f3de8
conformist-mw <conformist.mw@gmail.com>
2017-03-04 22:34:54 +0200

    added user detail view

--- instant/views.py
+++ instant/views.py
@@ -5,7 +5,16 @@ from .serializers import UserSerializer
 
 
 class UserList(APIView):
+
     def get(self, request, format=None):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+class UserDetail(APIView):
+
+    def get(self, request, pk, format=None):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
