e16dbd27858a5d98e718f171a23d6a9c6f0b0129
conformist-mw <conformist.mw@gmail.com>
2017-03-10 23:35:19 +0200

    boilerplate of react app

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,39 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var Hello = React.createClass({
+var UsersList = React.createClass({
+	loadUsersFromServer: function(){
+		$.ajax({
+			url: this.props.url,
+			datatype: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this)
+		})
+	},
+	getInitialState: function(){
+	    return {data: []};
+	},
+	componetDidMount: function(){
+		this.loadUsersFromServer();
+		setInterval(this.loadUsersFromServer, this.props.pollInterval)
+	},
 	render: function(){
+		if (this.state.data){
+			console.log('DATA!')
+			var userNodes = this.state.data.map(function(user){
+				return <li> {user.full_name} </li>
+			})
+		}
 		return (
-			<h1>
-			Hello, React!
-			</h1>
+			<div>
+				<h1>Hello React!</h1>
+				<ul>
+					{userNodes}
+				</ul>
+			</div>
 		)
 	}
 })
-
-ReactDOM.render(<Hello />, document.getElementById('container'))
+ReactDOM.render(<UsersList url='/instant/api/' pollInterval={1000} />, document.getElementById('container'))
