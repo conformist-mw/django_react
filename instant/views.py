9aca07d796accbfa5536eedef606c4e7bcb00b06
conformist-mw <conformist.mw@gmail.com>
2017-03-10 13:24:57 +0200

    added dynamic determine serializer class, add auth limit

--- instant/views.py
+++ instant/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 # from rest_framework.renderers import JSONRenderer
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, BaseUserSerializer
 
 
 class UserPagination(PageNumberPagination):
@@ -14,7 +14,11 @@ class UserPagination(PageNumberPagination):
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     # renderer_classes = [JSONRenderer]
-    permission_class = DjangoModelPermissionsOrAnonReadOnly
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     pagination_class = UserPagination
+
+    def get_serializer_class(self):
+        if not self.request.user.is_anonymous:
+            return UserSerializer
+        return BaseUserSerializer
