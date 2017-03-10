6fd775b1c3c9b4d4360ce914dd82083ef87a3e12
conformist-mw <conformist.mw@gmail.com>
2017-03-10 11:39:23 +0200

    remove unusable class, use pagination in view, permissions here too

--- instant/views.py
+++ instant/views.py
@@ -1,18 +1,20 @@
-from rest_framework import viewsets, renderers
+from rest_framework.viewsets import ModelViewSet
+# from rest_framework.renderers import JSONRenderer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    renderer_classes = [renderers.JSONRenderer]
-
-
-class UserDetail(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    renderer_classes = [renderers.JSONRenderer]
-
-    def get_queryset(self):
-        pk = self.kwargs['pk']
-        return User.objects.filter(pk=pk)
+    # renderer_classes = [JSONRenderer]
+    permission_class = DjangoModelPermissionsOrAnonReadOnly
+    pagination_class = UserPagination
