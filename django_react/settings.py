a5708d0c85a15874fdfe6cdd94241d3852cb5472
conformist-mw <conformist.mw@gmail.com>
2017-03-10 11:38:12 +0200

    comment out rest_framework options

--- django_react/settings.py
+++ django_react/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # REST FRAMEWORK
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
-}
+# REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    # ],
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 50
+# }
