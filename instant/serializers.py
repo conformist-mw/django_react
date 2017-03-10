206289793e0558371c816e5c57e6bdb28c0f3501
conformist-mw <conformist.mw@gmail.com>
2017-03-10 13:24:02 +0200

    added base view class with limit fields

--- instant/serializers.py
+++ instant/serializers.py
@@ -2,8 +2,16 @@ from .models import User
 from rest_framework import serializers
 
 
+class BaseUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('full_name', 'age', 'birth')
+
+
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('full_name', 'age', 'birth', 'email', 'ipv4', 'phone', 'street', 'city')
+        fields = ('full_name', 'age', 'birth', 'email',
+                  'ipv4', 'phone', 'street', 'city')
