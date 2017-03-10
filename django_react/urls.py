61469cfc7bacce58030568b0a38c05c0364284d5
conformist-mw <conformist.mw@gmail.com>
2017-03-10 23:39:40 +0200

    added route to index.html

--- django_react/urls.py
+++ django_react/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from instant.views import index
 
 urlpatterns = [
     url(r'^instant/', include('instant.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', index, name='index'),
 ]
