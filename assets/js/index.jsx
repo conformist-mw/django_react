56bbbf46646f3f9440832b9e2c979043be113a76
conformist-mw <conformist.mw@gmail.com>
2017-03-15 10:54:15 +0200

    initial fetch data from api, build index page, base route

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,39 +1,71 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-var UsersList = React.createClass({
-	loadUsersFromServer: function(){
-		$.ajax({
-			url: this.props.url,
-			datatype: 'json',
-			cache: false,
-			success: function(data){
-				this.setState({data: data});
-			}.bind(this)
-		})
-	},
-	getInitialState: function(){
-	    return {data: []};
-	},
-	componetDidMount: function(){
-		this.loadUsersFromServer();
-		setInterval(this.loadUsersFromServer, this.props.pollInterval)
-	},
-	render: function(){
-		if (this.state.data){
-			console.log('DATA!')
-			var userNodes = this.state.data.map(function(user){
-				return <li> {user.full_name} </li>
-			})
-		}
-		return (
-			<div>
-				<h1>Hello React!</h1>
-				<ul>
-					{userNodes}
-				</ul>
-			</div>
-		)
-	}
+let User = function(props){
+  return (
+    <div className='user'>
+      <div>Full Name: {props.user.full_name}</div>
+      <div>Age: {props.user.age}</div>
+      <div>Birth: {props.user.birth}</div>
+    </div>
+  )
+}
+
+let Users = React.createClass({
+  getInitialState: function(){
+    return {
+      link: 'http://localhost:8080/instant/api',
+      users: [],
+      previous: '',
+      next: '',
+      count: 0
+    }
+  },
+  componentDidMount: function(){
+    axios.get(this.state.link).then(xhr => {
+      this.setState({
+        users: xhr.data.results,
+        previous: xhr.data.previous,
+        next: xhr.data.next,
+        count: xhr.data.count
+      });
+      console.log(xhr)
+    })
+  },
+  render: function(){
+    return (
+      <div>
+        <h1>Our users are (overall count: {this.state.count}):</h1>
+        <Link to="other">other</Link>
+        <a href={this.state.next}>next</a>
+        <a href={this.state.previous}>prev</a>
+        {this.state.users.map(user => {
+          return <User user={user} key={user.id} />
+        })}
+      </div>
+    );
+  }
 })
-ReactDOM.render(<UsersList url='/instant/api/' pollInterval={1000} />, document.getElementById('container'))
+
+let Other = function(){
+  return (<div>
+            <h1>Other</h1>
+            <Link to="/">home</Link>
+          </div>
+  )
+}
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path="/" component={Users} />
+      <Route strict path="/other" component={Other} />
+    </div>
+  </Router>
+), document.querySelector('#container'))
