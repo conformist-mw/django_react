8363d30d21a4783d0efe20ab382c163baedb9050
conformist-mw <conformist.mw@gmail.com>
2017-03-17 16:38:42 +0200

    added production config for webpack

---
+++ webpack.prod.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+	context: __dirname,
+	entry: './assets/js/index',
+	output: {
+		path: path.resolve('./assets/dist/'),
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			minimize: true,
+			debug: false
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			beautify: false,
+			mangle: {
+				screw_ie8: true,
+				keep_fnames: true
+			},
+			compress: {
+				screw_ie8: true
+			},
+			comments: false
+		})
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		modules: ['node_modules']
+	}
+}
