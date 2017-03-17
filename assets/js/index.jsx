8a821ee5981441e51b1fa4227338724368026054
conformist-mw <conformist.mw@gmail.com>
2017-03-17 14:38:57 +0200

    implement base routes to specific user info

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory()
+
+let getUsers = function(){
+  return axios.get('http://localhost:8080/instant/api/?page=' + page)
+}
+
+let getUser = function(id){
+  return axios.get('http://localhost:8080/instant/api/' + id)
+}
 
 let User = function(props){
   return (
     <div className='user'>
-      <div>Full Name: {props.user.full_name}</div>
-      <div>Age: {props.user.age}</div>
-      <div>Birth: {props.user.birth}</div>
+      <div>Name: <Link to={'/profile/' + props.user.id}>{props.user.full_name}</Link></div>
     </div>
   )
 }
@@ -35,16 +40,13 @@ let Users = React.createClass({
         next: xhr.data.next,
         count: xhr.data.count
       });
-      console.log(xhr)
     })
   },
   render: function(){
     return (
       <div>
         <h1>Our users are (overall count: {this.state.count}):</h1>
-        <Link to="other">other</Link>
-        <a href={this.state.next}>next</a>
-        <a href={this.state.previous}>prev</a>
+        <hr />
         {this.state.users.map(user => {
           return <User user={user} key={user.id} />
         })}
@@ -53,19 +55,42 @@ let Users = React.createClass({
   }
 })
 
-let Other = function(){
-  return (<div>
-            <h1>Other</h1>
-            <Link to="/">home</Link>
-          </div>
-  )
-}
+let UserProfile = React.createClass({
+  getInitialState: function() {
+    return {
+      name: null,
+      age: null,
+      birth: null
+    }
+  },
+  componentDidMount: function() {
+    getUser(this.props.match.params.id).then(xhr => {
+      this.setState({
+        name: xhr.data.full_name,
+        age: xhr.data.age,
+        birth: xhr.data.birth
+      })
+    })
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>User Profile: {this.state.name}</h3>
+        <div>Age: {this.state.age}</div>
+        <div>Birth: {this.state.birth}</div>
+        <div>
+          <button onClick={history.goBack}>back</button>
+        </div>
+      </div>
+    )
+  }
+})
 
 ReactDOM.render((
   <Router>
     <div>
       <Route exact path="/" component={Users} />
-      <Route strict path="/other" component={Other} />
+      <Route path="/profile/:id" component={UserProfile} />
     </div>
   </Router>
 ), document.querySelector('#container'))
