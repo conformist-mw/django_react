22cd58751b986242697a7c73e80ffd1cf94cac98
conformist-mw <conformist.mw@gmail.com>
2017-03-18 12:40:45 +0200

    added routes to Key model methods

---
+++ sms/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^create/$', views.KeyViewSet.as_view({'get': 'create'})),
+    url(r'^last/$', views.KeyViewSet.as_view({'get': 'last'})),
+    url(r'^check/(?P<key>[a-zA-Z0-9]{4})/$', views.KeyViewSet.as_view({'get': 'retrieve'})),
+    url(r'^submit/(?P<key>[a-zA-Z0-9]{4})/$', views.KeyViewSet.as_view({'get': 'submit'}))
+]
