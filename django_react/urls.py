5d5d23802332440616aa9b82bf1148ab2e9963f3
conformist-mw <conformist.mw@gmail.com>
2017-03-18 12:40:10 +0200

    added url to sms app

--- django_react/urls.py
+++ django_react/urls.py
@@ -4,6 +4,7 @@ from instant.views import index
 
 urlpatterns = [
     url(r'^instant/', include('instant.urls')),
+    url(r'^sms/', include('sms.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^$', index, name='index'),
