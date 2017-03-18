a0c105ad996efe15c02b91a7d54e44ff7306e62f
conformist-mw <conformist.mw@gmail.com>
2017-03-18 12:42:10 +0200

    added serialiser to Key model

---
+++ sms/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Key
+
+
+class KeySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Key
+        fields = ['id', 'active']
