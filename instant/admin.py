8dcab5547bf4d5061c7be1e736c4e34cde46343c
conformist-mw <conformist.mw@gmail.com>
2017-03-21 21:42:26 +0200

    added admin view for User model

--- instant/admin.py
+++ instant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'age', 'birth', 'email', 'ipv4')
+    list_filter = ('birth',)
