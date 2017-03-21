32313079cf6a2ef07876cef101ae8f174e516cc6
conformist-mw <conformist.mw@gmail.com>
2017-03-21 21:41:48 +0200

    added login path

--- django_react/urls.py
+++ django_react/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from instant.views import index
 
 urlpatterns = [
     url(r'^instant/', include('instant.urls')),
     url(r'^sms/', include('sms.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.login),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^$', index, name='index'),
 ]
