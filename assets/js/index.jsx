5248a70b1c97b26bc5348baf85fb70d032d17246
conformist-mw <conformist.mw@gmail.com>
2017-03-22 16:26:56 +0200

    added classes to layout

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import createHistory from 'history/createBrowserHistory'
-
+import bootstrap from 'bootstrap'
 const history = createHistory()
 
 let getPage = function(link){
@@ -14,14 +14,10 @@ let getPage = function(link){
   }
 }
 
-let getUser = function(id){
-  return axios.get('/instant/api/' + id)
-}
-
 let User = function(props){
   return (
-    <div className='user'>
-      <div>Name: <Link to={'/profile/' + props.user.id}>{props.user.full_name}</Link></div>
+    <div className='user text-center'>
+      <Link to={'/profile/' + props.user.id}>{props.user.full_name}</Link>
     </div>
   )
 }
@@ -41,9 +37,7 @@ let Users = React.createClass({
     var link = this.state.link
     if(this.props.match.params.page){
       link = link + this.props.match.params.page 
-    }else{
-      link = link + this.state.current
-    }
+    }else{link = link + this.state.current}
     axios.get(link).then(xhr => {
       this.setState({
         users: xhr.data.results,
@@ -51,6 +45,8 @@ let Users = React.createClass({
         next: (xhr.data.next).split('=').pop(),
         count: xhr.data.count
       });
+    }).catch(error => {
+      console.log(error)
     })
   },
   componentWillReceiveProps(){
@@ -74,10 +70,10 @@ let Users = React.createClass({
   },
   render: function(){
     return (
-      <div>
+      <div className="users">
         <h1>Our users are (overall count: {this.state.count}):</h1>
-        <Link to={"/page/" + this.state.prev}>prev</Link>
-        <Link to={"/page/" + this.state.next}>next</Link>
+        <Link to={"/page/" + this.state.prev} className="prevLink"><span className="glyphicon glyphicon-chevron-left"></span></Link>
+        <Link to={"/page/" + this.state.next} className="nextLink"><span className="glyphicon glyphicon-chevron-right"></span></Link>
         <hr />
         {this.state.users.map(user => {
           return <User user={user} key={user.id} />
@@ -90,40 +86,60 @@ let Users = React.createClass({
 let UserProfile = React.createClass({
   getInitialState: function() {
     return {
-      name: null,
-      age: null,
-      birth: null
+      link: '/instant/api/',
     }
   },
   componentDidMount: function() {
-    getUser(this.props.match.params.id).then(xhr => {
+    var link = this.state.link
+    if(this.props.match.params.id){
+      link = link + this.props.match.params.id 
+    }else{link = link + this.state.current}
+    axios.get(link + "/").then(xhr => {
       this.setState({
         name: xhr.data.full_name,
         age: xhr.data.age,
-        birth: xhr.data.birth
+        birth: xhr.data.birth,
+        email: xhr.data.email,
+        ipv4: xhr.data.ipv4,
+        phone: xhr.data.phone,
+        street: xhr.data.street,
+        city: xhr.data.city
       })
     })
   },
   render: function() {
     return (
-      <div>
-        <h3>User Profile: {this.state.name}</h3>
-        <div>Age: {this.state.age}</div>
-        <div>Birth: {this.state.birth}</div>
-        <div>
-          <button onClick={history.goBack}>back</button>
+      <div className="userProfile">
+        <h3 className="text-center">{this.state.name}</h3>
+        <p><b>Age</b>: {this.state.age}</p>
+        <p><b>Birth</b>: {this.state.birth}</p>
+        {this.state.email != null && <p><b>Email</b>: {this.state.email}</p> }
+        {this.state.ipv4 != null && <p><b>ipv4</b>: {this.state.ipv4}</p> }
+        {this.state.phone != null && <p><b>phone</b>: {this.state.phone}</p> }
+        {this.state.street != null && <p><b>street</b>: {this.state.street}</p> }
+        {this.state.city != null && <p><b>city</b>: {this.state.city}</p> }
+        <div className="text-right div-btn">
+          <button type="button" className="btn btn-primary" onClick={history.goBack}> ← Go back</button>
         </div>
       </div>
     )
   }
 })
 
+let MainPage = function(){
+  return (
+    <div className='main'>
+      <div id="welcome"><Link to={'/page/1'}>Get started!</Link></div>
+    </div>
+  )
+};
+
 ReactDOM.render((
   <Router>
     <div>
-      <Route exact path="/" component={Users} />
+      <Route exact path="/" component={MainPage} />
       <Route path="/profile/:id" component={UserProfile} />
       <Route path="/page/:page" component={Users} />
     </div>
   </Router>
-), document.querySelector('#container'))
+), document.querySelector('#content'))
