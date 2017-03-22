6442fc8c5340ad12b6e405561d7ace0f155e5d72
conformist-mw <conformist.mw@gmail.com>
2017-03-22 16:28:57 +0200

    reduce page_size to 20

--- instant/views.py
+++ instant/views.py
@@ -8,7 +8,7 @@ from .serializers import UserSerializer, BaseUserSerializer
 
 
 class UserPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
