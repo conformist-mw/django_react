f3b0964014d67d81284ace19383282a5de86e17a
conformist-mw <conformist.mw@gmail.com>
2017-03-23 12:59:36 +0200

    added NODE_ENV to disable react prodaction warnings

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,6 +9,11 @@ module.exports = {
 		filename: '[name].bundle.js',
 	},
 	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: JSON.stringify("production")
+			}
+		}),
 		new webpack.LoaderOptionsPlugin({
 			minimize: true,
 			debug: false
