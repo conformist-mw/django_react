2c038ff0f51e3774bd9c4bf39546cca563fd29e9
conformist-mw <conformist.mw@gmail.com>
2017-03-26 23:22:51 +0300

    added route to index view

--- sms/urls.py
+++ sms/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^create/$', views.KeyViewSet.as_view({'get': 'create'})),
     url(r'^last/$', views.KeyViewSet.as_view({'get': 'last'})),
     url(r'^check/(?P<key>[a-zA-Z0-9]{4})/$', views.KeyViewSet.as_view({'get': 'retrieve'})),
-    url(r'^submit/(?P<key>[a-zA-Z0-9]{4})/$', views.KeyViewSet.as_view({'get': 'submit'}))
+    url(r'^submit/(?P<key>[a-zA-Z0-9]{4})/$', views.KeyViewSet.as_view({'get': 'submit'})),
+    url(r'^$', views.index),
 ]
