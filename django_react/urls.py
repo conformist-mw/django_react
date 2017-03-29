a5f250d13ef2b3fbe159602c7f9f6a02efcef19a
conformist-mw <conformist.mw@gmail.com>
2017-03-29 16:59:55 +0300

    added url to register page

--- django_react/urls.py
+++ django_react/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from instant.views import index
+from instant.views import index, register
 
 urlpatterns = [
+    url(r'^accounts/register/', register),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^instant/', include('instant.urls')),
     url(r'^sms/', include('sms.urls')),
