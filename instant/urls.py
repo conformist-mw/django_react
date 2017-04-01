5989bd9fdb0d7c5c0202b370ccab72bb08ccad88
conformist-mw <conformist.mw@gmail.com>
2017-04-01 16:01:53 +0300

    added support url

--- instant/urls.py
+++ instant/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, support
 
 router = routers.DefaultRouter()
 router.register(r'api', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^support/', support)
 ]
