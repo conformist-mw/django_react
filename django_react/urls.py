c816aec4951d667563083689ed3446f7c1e0c2a4
conformist-mw <conformist.mw@gmail.com>
2017-04-01 15:58:02 +0300

    added urls for register and userprofile. remove useless api-auth

--- django_react/urls.py
+++ django_react/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from instant.views import index, register
+from instant.views import index, register, get_user_profile
 
 urlpatterns = [
-    url(r'^accounts/register/', register),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/', register, name='register'),
+    url(r'^accounts/(?P<username>[a-zA-Z0-9]+)/$', get_user_profile),
     url(r'^instant/', include('instant.urls')),
     url(r'^sms/', include('sms.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^(?:.*)/?$', index, name='index'),
 ]
