3a8bb027d689614e3dde9ef318145f3d51e81567
conformist-mw <conformist.mw@gmail.com>
2017-04-04 20:16:03 +0300

    added feedback custom form with limited size attached image

--- instant/forms.py
+++ instant/forms.py
@@ -1,6 +1,59 @@
+from io import BytesIO
 from django import forms
+from django.template.defaultfilters import filesizeformat
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User as UserProfile
+
+MIN_SIZE = 51200
+MAX_SIZE = 2097152
+
+
+class RestrictedImageField(forms.ImageField):
+    default_error_messages = {
+        'invalid_image': (
+            'Некорректный формат файла, поддерживаемые форматы: JPG, BMP, PNG, TIFF'),
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.min_upload_size = MIN_SIZE
+        self.max_upload_size = MAX_SIZE
+        super(RestrictedImageField, self).__init__(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        data = super(RestrictedImageField, self).clean(*args, **kwargs)
+        if self.min_upload_size > data.size:
+            raise forms.ValidationError('Размер файла ({}) слишком мал для распознавания изображения. Минимум {}'.format(
+                filesizeformat(data.size), filesizeformat(self.min_upload_size)))
+        elif self.max_upload_size < data.size:
+            raise forms.ValidationError('Размер файла ({}) превышает максимальный размер. Максимум {}'.format(
+                filesizeformat(data.size), filesizeformat(self.max_upload_size)))
+        return data
+
+    def to_python(self, data):
+        f = super(RestrictedImageField, self).to_python(data)
+        if f is None:
+            return None
+        from PIL import Image
+        if hasattr(data, 'temporary_file_path'):
+            file = data.temporary_file_path()
+        else:
+            if hasattr(data, 'read'):
+                file = BytesIO(data.read())
+            else:
+                file = BytesIO(data['content'])
+        try:
+            image = Image.open(file)
+            if image.format not in ('JPEG', 'BMP', 'PNG', 'TIFF'):
+                raise forms.ValidationError(
+                    'Некорректный формат файла, поддерживаемые форматы: JPG, BMP, PNG, TIFF')
+            image.verify()
+            f.image = image
+            f.content_type = Image.MIME.get(image.format)
+        except Exception:
+            raise forms.ValidationError('Некорректный формат файла.')
+        if hasattr(f, 'seek') and callable(f.seek):
+            f.seek(0)
+        return f
 
 
 class UserCreateForm(UserCreationForm):
@@ -11,7 +64,8 @@ class UserCreateForm(UserCreationForm):
 
     class Meta:
         model = UserProfile
-        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password1',
+                  'password2', 'first_name', 'last_name')
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
@@ -19,3 +73,13 @@ class UserCreateForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class FeedBack(forms.Form):
+    first_name = forms.CharField(label='First Name', max_length=30)
+    last_name = forms.CharField(label='Last Name', max_length=30)
+    email = forms.EmailField(label='E-mail', max_length=254)
+    phone = forms.CharField(label='Phone Number', max_length=12)
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    image = RestrictedImageField()
