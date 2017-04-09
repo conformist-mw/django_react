f7ce7a3690746ea211955bf7fa32263657025d87
conformist-mw <conformist.mw@gmail.com>
2017-04-09 18:01:52 +0300

    added python social url, fix user profile links

--- django_react/urls.py
+++ django_react/urls.py
@@ -5,9 +5,10 @@ from instant.views import index, register, get_user_profile
 urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^accounts/register/', register, name='register'),
-    url(r'^accounts/(?P<username>[a-zA-Z0-9]+)/$', get_user_profile),
+    url(r'^accounts/(?P<username>.+)/$', get_user_profile),
     url(r'^instant/', include('instant.urls')),
     url(r'^sms/', include('sms.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('social_django.urls', namespace='social')),
     url(r'^(?:.*)/?$', index, name='index'),
 ]
