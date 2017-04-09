838c42cee1cc5551c93fef6cc72d52c082688c18
conformist-mw <conformist.mw@gmail.com>
2017-04-09 18:12:13 +0300

    replace send_mail with EmailMessage class, now after registration user automatically log in, fix register form -- return errors if errors, support view added name surname before email

--- instant/views.py
+++ instant/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_protect
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.renderers import JSONRenderer
 from rest_framework.pagination import PageNumberPagination
@@ -8,7 +7,8 @@ from django.contrib.auth.models import User as UserProfile
 from .models import User
 from .serializers import UserSerializer, BaseUserSerializer
 from .forms import UserCreateForm, FeedBack
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.contrib.auth import authenticate, login
 
 
 class UserPagination(PageNumberPagination):
@@ -33,16 +33,18 @@ def index(request):
     return render(request, 'instant/index.html')
 
 
-@csrf_protect
 def register(request):
     if request.method == 'POST':
         form = UserCreateForm(request.POST)
         if form.is_valid():
             form.save()
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password1'])
+            login(request, user)
             return redirect('/')
-    args = {}
-    args['form'] = UserCreateForm()
-    return render(request, 'registration/register.html', args)
+    else:
+        form = UserCreateForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 def support(request):
@@ -51,23 +53,24 @@ def support(request):
         if form.is_valid():
             name = form.cleaned_data['first_name']
             surname = form.cleaned_data['last_name']
-            phone = form.cleaned_data['phone']
-            email = form.cleaned_data['email']
+            email_from = form.cleaned_data['email']
+            email_from = '{} {} <{}>'.format(name, surname, email_from)
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
-            message = message + \
-                '\nFrom {} {}. Phone: {}'.format(name, surname, phone)
             recipient = ['conformist.mw@gmail.com']
-            send_mail(subject, message, email, recipient)
-            return redirect('/')
+            email = EmailMessage(subject, message, email_from, recipient)
+            email.attach(form.cleaned_data['image'].name,
+                         form.cleaned_data['image'].read(),
+                         form.cleaned_data['image'].content_type)
+            email.send()
+            return render(request, 'instant/success.html')
     else:
         if request.user.is_authenticated():
             form = FeedBack(initial={'first_name': request.user.first_name,
                                      'last_name': request.user.last_name,
-                                     'email': request.user.email})
+                                     })
             form.fields['first_name'].widget.attrs['disabled'] = True
             form.fields['last_name'].widget.attrs['disabled'] = True
-            form.fields['email'].widget.attrs['disabled'] = True
         else:
             form = FeedBack()
     return render(request, 'instant/support.html', {'form': form})
